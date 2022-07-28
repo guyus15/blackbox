2ebe04b65fe7aa3e30b07d46a89b2a0f7b99b624
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-07-28 14:58:21 +0100

    Created a function to return the logging directory

--- src/config.py
+++ src/config.py
@@ -4,21 +4,33 @@
 
 import json
 
+CONFIG_PATH = "config.json"
+
 # The configuration dictionary
 config = dict()
+
 
 ## Loads the configuration contained within the configuration file.
 #
 # Looks for a directory named in the configuration files
 # to store log files in. If it can't find this, it will
 # create one.
-def load_config(file: str):
+def load_config():
     global config
 
     # Don't bother loading configuration if it is already loaded.
     if len(config.keys()) > 0:
         return
 
-    with open(file) as fd:
+    with open(CONFIG_PATH) as fd:
         contents = fd.read()
         config = json.loads(contents)
+
+
+## Returns the path to the log directory.
+def get_log_dir():
+    global config
+
+    load_config()
+
+    return config["logging"]["log-directory"]
