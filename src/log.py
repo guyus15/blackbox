d4c629be67e0ead89b4aea85345686964f261c1a
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-07-28 16:00:54 +0100

    Created a function to write entries to files in the log directory

--- src/log.py
+++ src/log.py
@@ -3,7 +3,14 @@
 # @author Guy Chamberlain-Webber
 
 import os
+import sys
+import time
 import src.config as config
+
+
+## An exception raised when a platform type is not supported.
+class UnsupportedPlatformException (Exception):
+    pass
 
 
 ## Creates a directory to store log files.
@@ -19,3 +26,44 @@ def create_log_dir():
     else:
         print("Directory '{}' does not exist: creating...".format(log_path))
         os.mkdir(log_path)
+
+
+## Writes to a log entry within the log directory.
+#
+# This function determines whether to log on Windows or RPi, then will call
+# the correct write log function, after pre-pending time and date to the
+# log entry.
+#
+# @param entry The log entry.
+# @param logfile The log file.
+def write_log(entry: str, logfile: str):
+    current_time = time.strftime("%d/%m/%Y - %H:%M:%S", time.gmtime())
+    entry = current_time + entry
+
+    plat = sys.platform
+
+    if plat == "win32":
+        # Log on Windows
+        write_log_windows(entry, logfile);
+    elif plat == "linux":
+        # Log on Linux
+        write_log_linux(entry, logfile);
+    else:
+        # Unsupported platform
+        raise UnsupportedPlatformException("The blackbox does not support platforms of type '{}'.".format(plat))
+
+
+## Writes to a log entry within the log directory on Windows.
+#
+# @param entry The log entry.
+# @param logfile The log file.
+def write_log_windows(entry: str, logfile: str):
+    pass
+
+
+## Writes to a log entry within the log directory on Linux.
+#
+# @param entry The log entry.
+# @param logfile The log file.
+def write_log_linux(entry: str, logfile: str):
+    pass
