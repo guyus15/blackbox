c959f481bcabf9cc3c30dee0b9b8b63eb2560d3e
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-07-28 16:54:50 +0100

    Create a test case for the create_log_dir when a log directory already exists

--- tests/log_test.py
+++ tests/log_test.py
@@ -7,11 +7,14 @@ import os
 import src.config as config
 import src.log as log
 
+from unittest.mock import patch
+from io import StringIO
+
 
 ## This test case test the 'log' module.
 class TestLog(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
+
+    def setUp(self):
         # Remove any test log directories if they exist.
         log_dir = config.get_log_dir()
 
@@ -28,3 +31,17 @@ class TestLog(unittest.TestCase):
         self.assertFalse(os.path.exists(log_dir))
         log.create_log_dir()
         self.assertTrue(os.path.exists(log_dir))
+
+    # Test 2
+    def test_create_log_dir_exist(self):
+        # This test ensures that the create_log_dir() function correctly informs the
+        # user which log directory is being used if already one exists.
+
+        log_dir_name = config.get_log_dir()
+
+        # Create a log directory
+        log.create_log_dir()
+
+        with patch("sys.stdout", new=StringIO()) as fake_output:
+            log.create_log_dir()
+            self.assertEqual(fake_output.getvalue(), "Logging to directory '{}'\n".format(log_dir_name))
