52df37e68b49b8e2eb7a1a526e32da9dcde03b6e
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-02 09:26:54 +0100

    Replaced use of NotImplementedException with Python's ABC abstract methods

--- src/packet/byte_container.py
+++ src/packet/byte_container.py
@@ -3,23 +3,29 @@
 # inside a class.
 # @author Guy Chamberlain-Webber
 
+import abc
+
+
 ## An interface containing specific byte-related functions.
 #
 # Python does not officially support interfaces, so we use a NotImplemented
 # exception to ensure that functions are defined elsewhere.
-class IByteContainer:
+class IByteContainer(abc.ABC):
     ## Gets an object as an array of bytes.
     #
     # @return The containing object as an array of bytes.
+    @abc.abstractmethod
     def get_byte_array(self):
-        raise NotImplementedError("@get_byte_array has not been implemented.")
+        pass
 
     # Sets the parameter contained at 'key' to 'value'.
+    @abc.abstractmethod
     def set_parameter(self, key, value):
-        raise NotImplementedError("@set_parameter has not been implemented.")
+        pass
 
     # Gets the parameter contained at 'key'
     #
     # @return The parameter contained at the specified key.
+    @abc.abstractmethod
     def get_parameter(self, key):
-        raise NotImplementedError("@get_parameter has not been implemented.")
+        pass
