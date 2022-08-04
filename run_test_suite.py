0e067c40e76a000c9dc3980e2aa69dcc33815dfd
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-04 16:22:50 +0100

    Modified the test loader to use values from the configuration file

--- run_test_suite.py
+++ run_test_suite.py
@@ -2,6 +2,7 @@
 # @brief A test suite designed to run all existing test cases.
 # @author Guy Chamberlain-Webber
 
+import src.config as config
 import unittest
 from unittest import TestLoader
 
@@ -9,7 +10,11 @@ from unittest import TestLoader
 ## Runs tests found in the 'test' directory configured in config.json
 def run_tests():
     loader = TestLoader()
-    suite = loader.discover(start_dir="tests/", pattern="*_test.py", top_level_dir=None)
+
+    start_directory = config.get_test_start_directory()
+    discovery_pattern = config.get_test_discovery_pattern()
+
+    suite = loader.discover(start_dir=start_directory, pattern=discovery_pattern, top_level_dir=None)
 
     unittest.TextTestRunner().run(suite)
 
