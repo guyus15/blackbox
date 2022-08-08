af9cbb3a96a522056503d555448818ae4a6be155
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-08 13:44:52 +0100

    Added a get_byte_array() override so SOH, SEQ and checksum can be added to each packet

--- src/packet/packet.py
+++ src/packet/packet.py
@@ -6,6 +6,9 @@ from src.packet.byte_container import IByteContainer
 from src.packet.headers import BaseHeader
 from src.packet.content import Content
 
+soh = 0x01
+seq = 0x01
+
 
 ## Provides an abstraction of a data packet, which will be able to be transmitted to, and received
 # from the panel.
@@ -21,3 +24,25 @@ class Packet(Content):
         _params_copy = self._params
         self._params = header.get_parameters()
         self._params.update(_params_copy)
+
+    ## Gets an object as an array of bytes.
+    #
+    # The packet will provide SOH and SEQ numbers to the byte array, as well
+    # as a checksum at the end based on all previous bytes.
+    #
+    # @return The containing object as an array of bytes.
+    def get_byte_array(self) -> list:
+        default_array = super().get_byte_array()
+
+        checksum = 0
+        for byte in default_array:
+            checksum += byte
+
+        # Modulus operation to ensure checksum can be contained within one byte.
+        checksum %= 255
+
+        default_array.insert(0, soh)
+        default_array.insert(1, seq)
+        default_array.append(checksum)
+
+        return default_array
