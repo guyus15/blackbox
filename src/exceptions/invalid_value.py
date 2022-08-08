fc13b3e74698c2cd1b05e6e380e124a909d0ea9d
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-08 15:43:27 +0100

    Created the invalid value exception

---
+++ src/exceptions/invalid_value.py
@@ -0,0 +1,8 @@
+## @file invalid_value.py
+# @brief Contains InvalidValueException which is raised when a given value in the configuration file
+# is invalid.
+# @author Guy Chamberlain-Webber
+
+# An exception raised when a value is invalid in the configuration file.
+class InvalidValueException(Exception):
+    pass
