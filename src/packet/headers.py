dbea211bf9af6283d3ae57dcb2f57ff9fc46e8fa
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-09 09:59:05 +0100

    Added default value overriding to headers as well as corresponding tests

--- src/packet/headers.py
+++ src/packet/headers.py
@@ -5,7 +5,8 @@
 import src.config as config
 from src.config import MXSpeakVersion
 from src.packet.content import Content
-
+from src.exceptions.parameter_not_found import ParameterNotFoundException
+from src.exceptions.invalid_value import InvalidValueException
 
 ## Base header class.
 class BaseHeader(Content):
@@ -23,7 +24,7 @@ class BaseHeader(Content):
 
 ## An MX Speak 5 Local Header, which is used when connected to the panel's COM2 serial port.
 class LocalHeaderMX5(BaseHeader):
-    def __init__(self, packet_id: int):
+    def __init__(self, packet_id: int, **kwargs):
         params = {
             "packet_length":                config.get_packet_length(MXSpeakVersion.MX_SPEAK5),
             "network_node":                 0x00,
@@ -38,10 +39,17 @@ class LocalHeaderMX5(BaseHeader):
 
         super().__init__(**params)
 
+        # Override defaults if they have been provided.
+        for attr in kwargs:
+            if self.check_exists(attr):
+                self._params[attr] = kwargs[attr]
+            else:
+                raise ParameterNotFoundException(f"Given parameter '{attr}' does not exist in header.")
+
 
 ## An MX Speak 6 Local Header, which is used when connected to the panel's COM2 serial port.
 class LocalHeaderMX6(BaseHeader):
-    def __init__(self, packet_id: int):
+    def __init__(self, packet_id: int, **kwargs):
         params = {
             "packet_length":                config.get_packet_length(MXSpeakVersion.MX_SPEAK6),
             "mx_speak_signature":           config.get_mx_signature(),
@@ -57,3 +65,13 @@ class LocalHeaderMX6(BaseHeader):
         }
 
         super().__init__(**params)
+
+        # Override defaults if they have been provided.
+        for attr in kwargs:
+            if attr == "mx_speak_signature":
+                raise InvalidValueException(f"Invalid value '{attr}': Cannot modify MX Speak Signature value.")
+
+            if self.check_exists(attr):
+                self._params[attr] = kwargs[attr]
+            else:
+                raise ParameterNotFoundException(f"Given parameter '{attr}' does not exist in header.")
--- tests/packet/headers_test.py
+++ tests/packet/headers_test.py
@@ -4,6 +4,8 @@
 
 import unittest
 from src.packet.headers import LocalHeaderMX5, LocalHeaderMX6
+from src.exceptions.parameter_not_found import ParameterNotFoundException
+from src.exceptions.invalid_value import InvalidValueException
 
 
 ## This test case tests the 'headers' file and classes.
@@ -31,3 +33,68 @@ class TestHeaders(unittest.TestCase):
         expected_value = [11, 228, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         self.assertEqual(mx6_header.get_byte_array(), expected_value)
+
+    # Test 3
+    def test_mx5_override_default_contents(self):
+        # This test ensures that it is possible to override an MX5 header's contents
+        # if further arguments are provided in the initialiser.
+
+        mx5_header = LocalHeaderMX5(182,
+                                    network_node=10,
+                                    channel=5,
+                                    destination_task=1
+                                    )
+
+        expected_value = [9, 10, 5, 0, 1, 0, 0, 0, 182]
+
+        self.assertEqual(mx5_header.get_byte_array(), expected_value)
+
+    # Test 4
+    def test_mx5_override_non_existent_attr(self):
+        # This test ensures that it is not possible to add new values to the MX5 header
+        # by adding additional arguments. A ParameterNotFoundException should be raised.
+
+        with self.assertRaises(ParameterNotFoundException) as cm:
+            mx5_header = LocalHeaderMX5(182, non_existent=10, another=15, yet_another=20)
+
+        exception = cm.exception
+        self.assertIsNotNone(exception)
+
+    # Test 5
+    def test_mx6_override_default_contents(self):
+        # This test ensures that it is possible to override an MX6 header's contents
+        # if further arguments are provided to the initialiser.
+
+        mx6_header = LocalHeaderMX6(149,
+                                    network_node=1,
+                                    channel=10,
+                                    destination_task=5
+                                    )
+
+        expected_value = [11, 228, 1, 10, 0, 5, 0, 0, 0, 149, 0]
+
+        self.assertEqual(mx6_header.get_byte_array(), expected_value)
+
+    # Test 6
+    def test_mx6_override_speak_signature(self):
+        # This test ensures that it is not possible to override the MX6 Speak Signature,
+        # if it is attempted, an InvalidValueException will be raised.
+
+        with self.assertRaises(InvalidValueException) as cm:
+            mx6_header = LocalHeaderMX6(150,
+                                        mx_speak_signature=200
+                                        )
+
+        exception = cm.exception
+        self.assertIsNotNone(exception)
+
+    # Test 7
+    def test_mx6_override_non_existent_attr(self):
+        # This test ensures that it is not possible to add new values to the MX6 header
+        # by adding additional arguments. A ParameterNotFoundException should be raised.
+
+        with self.assertRaises(ParameterNotFoundException) as cm:
+            mx6_header = LocalHeaderMX6(150, non_existent=10, another=15, yet_anothe=20)
+
+        exception = cm.exception
+        self.assertIsNotNone(exception)
