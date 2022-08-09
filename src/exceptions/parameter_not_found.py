71091bd305aca3eecfe152998bb5f84d3746b6ee
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-09 09:39:53 +0100

    Added ParameterNotFoundException

---
+++ src/exceptions/parameter_not_found.py
@@ -0,0 +1,7 @@
+## @file parameter_not_found.py
+# @brief Contains ParameterNotFounException which is raised when particular parameter can not be found.
+# @author Guy Chamberlain-Webber
+
+## An exception thrown when a particular parameter can not be found.
+class ParameterNotFoundException(Exception):
+    pass
