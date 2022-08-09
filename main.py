1df7be019cb3444460504eaee3a6d09fac8b5a54
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-09 10:03:53 +0100

    Added the creation and sending of a test restart packet

--- main.py
+++ main.py
@@ -2,9 +2,30 @@
 # @brief The main entry point for the program.
 # @author Guy Chamberlain-Webber
 
+import serial
+import src.config as config
+
+from src.packet.packet import Packet
+from src.packet.headers import LocalHeaderMX5
+
 
 def run():
-    pass
+    # Creating and sending a panel restart packet.
+    header = LocalHeaderMX5(182,
+                            channel=0x06,
+                            destination_channel_address=0x1d,
+                            source_channel_address=0x1d
+                            )
+    new_packet = Packet(header)
+
+    with serial.Serial(port=config.get_com_port(),
+                       baudrate=config.get_baudrate(),
+                       timeout=config.get_timeout(),
+                       bytesize=config.get_bytesize(),
+                       parity=config.get_parity(),
+                       stopbits=config.get_stopbits()) as ser:
+
+        ser.write(new_packet.get_byte_array())
 
 
 if __name__ == "__main__":
