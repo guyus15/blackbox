ae82e916ea4563a0a284cd4540300202cd047948
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-09 11:13:32 +0100

    Added IWritable interface

---
+++ src/packet/writable.py
@@ -0,0 +1,13 @@
+## @file.py writable.py
+# @brief Contains interface IWritable used to enforce writing functionality on an object.
+# @author Guy Chamberlain-Webber
+
+import abc
+
+
+## An interface containing specific writing functions.
+class IWritable(abc.ABC):
+    ## Writes to a serial communications port.
+    @abc.abstractmethod
+    def write(self):
+        pass
