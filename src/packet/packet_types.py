3491f4333555687aa7e31ad7b82ecb5322802024
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-10 15:57:54 +0100

    Removed redundant information from the concrete packet classes

--- src/packet/packet_types.py
+++ src/packet/packet_types.py
@@ -2,21 +2,29 @@
 # @brief Contains concrete implementations of frequently used packets.
 # @author Guy Chamberlain-Webber
 
+from src.packet.headers import LocalHeaderMX5
 from src.packet.packet import Packet
-from src.packet.headers import LocalHeaderMX5, LocalHeaderMX6
 from src.packet.packet_ids import PacketID
 from src.packet.writable import IWritable
 
 
+## A class representing a restart panel packet.
 class RestartPanelMX5(IWritable):
     def __init__(self):
-        header = LocalHeaderMX5(PacketID.RESTART_REQUEST,
-                                channel=0x06,
-                                destination_channel_address=0x1d,
-                                source_channel_address=0x1d
-                                )
+        header = LocalHeaderMX5(PacketID.RESTART_REQUEST)
         self.packet = Packet(header)
 
     ## Writes to a serial communications port.
+    def write(self):
+        self.packet.write()
+
+
+## A class representing a panel information request.
+class PanelDetailsRequestMX5(IWritable):
+    def __init__(self):
+        header = LocalHeaderMX5(PacketID.PANEL_DETAILS_REQUEST)
+        self.packet = Packet(header)
+
+    ## Writres to a serial communications port.
     def write(self):
         self.packet.write()
