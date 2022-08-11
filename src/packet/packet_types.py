a56c1ce353db98c4b78475d0db011d05e6fd6604
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-11 09:33:22 +0100

    Added MX6 variants of existing concrete packets

--- src/packet/packet_types.py
+++ src/packet/packet_types.py
@@ -3,12 +3,13 @@
 # @author Guy Chamberlain-Webber
 
 from src.packet.headers import LocalHeaderMX5
+from src.packet.headers import LocalHeaderMX6
 from src.packet.packet import Packet
 from src.packet.packet_ids import PacketID
 from src.packet.writable import IWritable
 
 
-## A class representing a restart panel packet.
+## A class representing a restart panel packet (MX5).
 class RestartPanelMX5(IWritable):
     def __init__(self):
         header = LocalHeaderMX5(PacketID.RESTART_REQUEST)
@@ -19,12 +20,34 @@ class RestartPanelMX5(IWritable):
         self.packet.write()
 
 
-## A class representing a panel information request.
+## A class representing a restart panel packet (MX6).
+class RestartPanelMX6(IWritable):
+    def __init__(self):
+        header = LocalHeaderMX5(PacketID.RESTART_REQUEST)
+        self.packet = Packet(header)
+
+    ## Writes to a serial communication port.
+    def write(self):
+        self.packet.write()
+
+
+## A class representing a panel information request (MX5).
 class PanelDetailsRequestMX5(IWritable):
     def __init__(self):
         header = LocalHeaderMX5(PacketID.PANEL_DETAILS_REQUEST)
         self.packet = Packet(header)
 
-    ## Writres to a serial communications port.
+    ## Writes to a serial communications port.
+    def write(self):
+        self.packet.write()
+
+
+## A class representing a panel information request (MX6).
+class PanelDetailsRequestMX6(IWritable):
+    def __init__(self):
+        header = LocalHeaderMX6(PacketID.PANEL_DETAILS_REQUEST)
+        self.packet = Packet(header)
+
+    # Writes to a serial communications port.
     def write(self):
         self.packet.write()
