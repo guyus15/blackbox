f2d8f80d7a0a815868d6eab069eaac9402b6eea7
Guy Rhodri James Chamberlain-Webber <jchambgu@jci.com>
2022-08-12 08:59:43 +0100

    Created constants file

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+## @file constants.py
+# @brief Contains constants used throughout the program, and are modifiable if necessary.
+# @author Guy Chamberlain-Webber
+
+SOH = 0x01  # Start of frame
+ACK = 0x06  # Acknowledge
+SEQ_WRAP = 0x0f  # Number to wrap the SEQ
